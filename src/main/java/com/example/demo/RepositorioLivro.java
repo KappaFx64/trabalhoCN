c9d6f1de9eee517b7ddec076c334cf74238c30f7
KappaFx64 <116957656+KappaFx64@users.noreply.github.com>
2024-04-06 19:47:43 +0100

    Update RepositorioLivro.java

--- src/main/java/com/example/demo/RepositorioLivro.java
+++ src/main/java/com/example/demo/RepositorioLivro.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.azure.spring.data.cosmos.repository.CosmosRepository;
 import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
+import java.util.*;
 
 @EnableCosmosRepositories
 public interface RepositorioLivro extends CosmosRepository<Livro, String> {
